2c3ae4f1ca467dfbb1d7f9c51c5e110f1f55c86a
Shashank <shashankverma2017@gmail.com>
2021-06-13 07:22:41 +0530

    newsletter modal added in shopping web template

--- Shopping Webpage/js/app.js
+++ Shopping Webpage/js/app.js
@@ -29,3 +29,28 @@ document.getElementById("jeans").addEventListener("click",showDropdown);
 function showDropdown(){
     document.querySelector(".jeans-category").classList.toggle("show-jeans-category");
 }
+
+// newsletter Starts
+const newsletter = document.getElementById("newsletter");
+newsletter.addEventListener("click",function(){
+    // console.log("newsletter Clicked");
+    document.querySelector(".modal-overlay").classList.add("open-modal");
+});
+document.getElementById("newsletter-cross-btn").addEventListener("click",function(){
+    document.querySelector(".modal-overlay").classList.remove("open-modal");
+});
+document.getElementById("newsletter-submit-button").addEventListener("click",function(){
+    document.querySelector(".modal-overlay").classList.remove("open-modal");
+});
+// Newsletter Ends
+
+// Items
+
+const items = [
+    {
+        id:1,
+        img: 'img/jeans1.jpg',
+        item: 'Ripped Jeans',
+        price: '$24.99'
+    }
+]
